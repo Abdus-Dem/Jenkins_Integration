a5d420050c00e8be6f1ec7002f16457fe6fdc8b2
Abdus-Dem <abdus.demiryurek@gmail.com>
2020-06-03 07:26:31 -0400

    first commit

---
+++ src/test/Selenium_Jenkins_Integration.java
@@ -0,0 +1,60 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Selenium_Jenkins_Integration {
+
+	
+		
+		WebDriver driver;
+		
+		
+		@BeforeMethod
+		public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"/Users/user1/Documents/selenium dependencies/drivers/chromedriver");
+		
+		 driver = new ChromeDriver();
+		
+		driver.get("http://google.com");
+		
+		}
+		
+		
+		@Test
+		public void test() {
+			
+			
+			WebElement searchBox = driver.findElement(By.name("q"));
+			
+			searchBox.sendKeys("porsche" + Keys.ENTER);
+			
+			Assert.assertTrue(driver.getTitle().contains("porsche"));
+			
+			
+			
+			
+		}
+		
+		
+		@AfterMethod
+		public void tearDown() {
+			
+			driver.quit();
+			
+		}
+		
+		
+
+	}
+
+
